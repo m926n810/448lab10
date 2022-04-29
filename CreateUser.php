b0e97bf8e11c198abe2cbc4ed7f30666552165c2
m926n810 <m926n810@ku.edu>
2022-04-29 17:02:24 -0500

    why not working

--- CreateUser.php
+++ CreateUser.php
@@ -1,12 +1,4 @@
 <?php 
-$myqsli = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
-if ($mysqli->connect_error) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-$userid = $_POST("user_id");
-$query = "INSERT INTO User ('$userid')";
-
 echo '<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +10,15 @@ echo '<!DOCTYPE html>
 </head>
 <body>
     <div class="center"><center>';
-        if($myqsli->query($query)===TRUE) echo "<h3> Created user $userid successfully!</h3>";
-        else echo "ERROR: ".$mysqli->error;
+        $myqsli = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
+        if ($mysqli->connect_error) {
+            echo "<h3>Connect failed:<br> $mysqli->connect_error </h3> ";
+        }else{
+            $userid = $_POST("user_id");
+            $query = "INSERT INTO User ('$userid')";
+            
+            if($myqsli->query($query)===TRUE) echo "<h3> Created user $userid successfully!</h3>";
+            else echo "ERROR: ".$mysqli->error;
+        }
     echo '</div></center>';
 ?>
