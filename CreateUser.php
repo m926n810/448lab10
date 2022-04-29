0a21d5dd90077f46d37e9e578c8559ca3322d787
m926n810 <m926n810@ku.edu>
2022-04-29 16:48:04 -0500

    Ex 1,2 done

---
+++ CreateUser.php
@@ -0,0 +1,24 @@
+<?php 
+$myqsli = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+$userid = $_POST("user_id");
+$query = "INSERT INTO User ($userid)";
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message Board system</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="center"><center>';
+        if($myqsli->query($query)===TRUE) echo "<h3> Created user $userid successfully!</h3>";
+        else echo "ERROR: ".$mysqli->error;
+    echo '</div></center>';
+?>
