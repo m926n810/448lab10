0857354e8ca2cfefe51ba0a7f8f7e7ff92498e0a
m926n810 <m926n810@ku.edu>
2022-04-29 17:13:50 -0500

    it should work

--- CreateUser.php
+++ CreateUser.php
@@ -12,10 +12,11 @@ echo '<!DOCTYPE html>
     <div class="center"><center>';
         $myqsli = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
         if ($mysqli->connect_error) {
-            echo "<h3>Connect failed:<br> $mysqli->connect_error </h3> ";
+            die("Connect failed: ".$myqsli->connect_error);
         }else{
             $userid = $_POST("user_id");
-            $query = "INSERT INTO User ('$userid')";
+            $query = "  INSERT INTO User (user_id) 
+                        VALUES ($userid)";
             
             if($myqsli->query($query)===TRUE) echo "<h3> Created user $userid successfully!</h3>";
             else echo "ERROR: ".$mysqli->error;
