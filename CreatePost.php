824d19d077dd5aad3f83eb09b35da81e731f8a3e
m926n810 <m926n810@ku.edu>
2022-04-29 23:21:04 -0500

    asddsadasasd

--- CreatePost.php
+++ CreatePost.php
@@ -11,15 +11,19 @@ echo '<head>
         }else{
             $userid = $_POST["user_id"];
             $post = $_POST["post"];
+            $validate = FALSE;
 
-            $validate_user = "SELECT user_id FROM Users WHERE user_id = '".$userid."'";
-            $query = "  INSERT INTO Post (post_id, content, author_id) 
-                        VALUES (NULL, '$post','$userid')";
-            echo $validate_user;
-            if($mysqli.query($validate_user)) {
+            $validate_user = "SELECT user_id FROM Users";
+            $query = "INSERT INTO Post (post_id, content, author_id) VALUES (NULL, '$post','$userid')";
+
+            if($result = $mysqli->query($validate_user)) {
+                while($row = $result->fetch_assoc()){
+                    if($row["user_is"] == $userid) $validate = TRUE;
+                }
+            $result->free();
+            if($validate){
                 if($myqsli->query($query)===TRUE) echo " <h3>Created user $userid's post successfully!</h3>";
                 else echo "<h3>ERROR: ".$mysqli->error."</h3>";
-                $validate_result->free();
             }else echo "<h3>User $userid does not exist in the database!</h3>";
             $myqsli->close();
         }
