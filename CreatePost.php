63e0cbff016bbb35e3c38f1f95cdd5038ed4beec
m926n810 <m926n810@ku.edu>
2022-04-29 23:28:56 -0500

    ssssss

--- CreatePost.php
+++ CreatePost.php
@@ -5,7 +5,7 @@ echo '<head>
 </head>
 <body>
     <div class="center"><center>';
-        $myqsli = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
+        $database = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
         if ($mysqli->connect_error) {
             die("Connect failed: ".$myqsli->connect_error);
         }else{
@@ -13,20 +13,20 @@ echo '<head>
             $post = $_POST["post"];
             $validate = FALSE;
 
-            $validate_user = "SELECT user_id FROM Users";
+            $validateuser = "SELECT user_id FROM Users";
             $query = "INSERT INTO Post (post_id, content, author_id) VALUES (NULL, '$post','$userid')";
 
-            if($result = $mysqli->query($validate_user)) {
+            if($result = $database->query($validateuser)) {
                 while($row = $result->fetch_assoc()){
-                    if($row["user_is"] == $userid) $validate = TRUE;
+                    if($row["user_id"] == $userid) $validate = TRUE;
                 }
                 $result->free();
             }
             if($validate){
                 if($myqsli->query($query)===TRUE) echo " <h3>Created user $userid's post successfully!</h3>";
-                else echo "<h3>ERROR: ".$mysqli->error."</h3>";
+                else echo "<h3>ERROR: ".$database->error."</h3>";
             }else echo "<h3>User $userid does not exist in the database!</h3>";
-            $myqsli->close();
+            $database->close();
         }
         echo '</div></center></body></html>';
 ?>
