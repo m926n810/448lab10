33d8706f5a6854f938c0bece30fcdef2ad6a89d5
m926n810 <m926n810@ku.edu>
2022-04-29 23:45:43 -0500

    hope this work

--- CreatePost.php
+++ CreatePost.php
@@ -14,13 +14,20 @@ echo '<head>
             $validate = FALSE;
 
             $validateuser = "SELECT user_id FROM Users WHERE user_id = '$userid'";
+            $query = "INSERT INTO Post (post_id,content,author_id) VALUES (NULL,'$post','$userid')";
 
             if($result = $database->query($validateuser)) {
                 while($row = $result->fetch_assoc()){
-                   echo "<h3>".$row["user_id"]."</h3>";
+                    if($row["user_id"] == $userid) $validate = TRUE;
                 }
                 $result->free();
             }
+
+            if($validate){
+                if($myqsli->query($query)===TRUE) echo " <h3>Created user $userid's post successfully!</h3>";
+                else echo "<h3>ERROR: ".$database->error."</h3>";
+            }else echo "<h3>User $userid does not exist in the database!</h3>";
+
             $database->close();
         }
         echo '</div></center></body></html>';
