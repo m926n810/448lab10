f1bc557365ab0e034a9451c4a65d380b3f26a261
m926n810 <m926n810@ku.edu>
2022-05-02 18:59:46 -0500

    ssssss

--- Viewpost.php
+++ Viewpost.php
@@ -6,7 +6,7 @@ echo '<head>
 <body>
     <div class="center"><center>
     <h1>Message Board system</h1>
-    <h3>User list: </h3>';
+    <h3>Contents list: </h3>';
         $database = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
         if ($database->connect_error) {
             die("Connect failed: ".$myqsli->connect_error);
