c5ff9d5d74cb2022d5f28358eef1bbbbe32dc29d
m926n810 <m926n810@ku.edu>
2022-05-02 19:59:05 -0500

    important one

--- Deletepost.php
+++ Deletepost.php
@@ -6,18 +6,20 @@ echo '<head>
 <body>
     <div class="center"><center>
     <h1>Message Board system</h1>
-    <h3>Contents list: </h3>';
+    <h3>Deleted post(s):</h3>';
         $database = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
         if ($database->connect_error) {
             die("Connect failed: ".$myqsli->connect_error);
         }else{
             $result = $database->query("SELECT * FROM Post");
-            $query="";
             for($i=1;$i<=$result->num_rows;$i++){
                 $delete = $_POST["$i"];
-                if($delete == $i) $query .="DELETE FROM Post WHERE post_id = '$delete'";
+                if($delete == $i){
+                    $query ="DELETE FROM Post WHERE post_id = '$delete'";
+                    if($database->query($query)===TRUE) echo "<h5>$i</h5>";
+                    else echo "<h5>Error: ???</h5>";
+                }
             }
-            echo $query;
             $database->close();
         }
         echo '</center></div></body></html>';
