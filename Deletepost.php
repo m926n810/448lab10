ec146730a5aab4f9303125056d4ec6392d301c53
m926n810 <m926n810@ku.edu>
2022-05-02 19:24:47 -0500

    asddsasad

---
+++ Deletepost.php
@@ -0,0 +1,19 @@
+<?php 
+echo '<head>
+    <title>Message Board system</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="center"><center>
+    <h1>Message Board system</h1>
+    <h3>Contents list: </h3>';
+        $database = new mysqli("mysql.eecs.ku.edu","m926n810","Yi7saine","m926n810");
+        if ($database->connect_error) {
+            die("Connect failed: ".$myqsli->connect_error);
+        }else{
+            $delete = $_POST["delete"];
+            echo $delete;
+            $database->close();
+        }
+        echo '</center></div></body></html>';
+?>
